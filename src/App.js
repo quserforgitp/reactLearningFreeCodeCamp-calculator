0d0839a26f0a1cf805162f59e373128d3dda74ea
Helios <adealumnonegligas@gmail.com>
2023-02-16 21:15:24 -0600

    logo estilizado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+import FreeCodeCampLogo from "./img/FreeCodeCamp_logo.png"
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="freecodecamp-logo-contenedor">
+        <img 
+          src={FreeCodeCampLogo}
+          className="freecodecamp-logo"
+          alt="logo de FreeCodeCamp"
+        />
+      </div>
     </div>
   );
 }

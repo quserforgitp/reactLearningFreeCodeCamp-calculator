d6924c05ff1cd5f19460427b5ff3e110e9d92019
Helios <adealumnonegligas@gmail.com>
2023-02-16 22:08:20 -0600

    Pantalla estilizada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import FreeCodeCampLogo from "./img/FreeCodeCamp_logo.png"
 import Boton from "./components/Boton";
+import Pantalla from "./components/Pantalla";
 
 function App() {
   return (
@@ -12,14 +13,31 @@ function App() {
           alt="logo de FreeCodeCamp" />
       </div>
       <div className="contenedor-calculadora">
+        <Pantalla></Pantalla>
         <div className="fila">
           <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
           <Boton>+</Boton>
         </div>
-        <div className="fila"></div>
-        <div className="fila"></div>
-        <div className="fila"></div>
-        <div className="fila"></div>
+        <div className="fila">
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className="fila">
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className="fila">
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
         <div className="fila"></div>
       </div>
     </div>
